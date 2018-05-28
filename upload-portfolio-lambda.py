6051a2928d8bd03e2b0054f448e58d84b183231e
Jeff Carley <jeff@thecarleys.net>
2018-05-28 15:44:30 -0600

    Update portfolio deployment function for lambda

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -3,17 +3,21 @@ import io
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3')
+def lambda_handler(event, context):
 
-portfolio_bucket = s3.Bucket('portfolio.net17research.com')
-build_bucket = s3.Bucket('portfoliobuild.net17research.com')
+    s3 = boto3.resource('s3')
 
-portfolio_zip = io.BytesIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    portfolio_bucket = s3.Bucket('portfolio.net17research.com')
+    build_bucket = s3.Bucket('portfoliobuild.net17research.com')
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm,
-          ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    portfolio_zip = io.BytesIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj, nm,
+              ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    return 'Update Complete'
