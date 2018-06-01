aa55c04a06d616f80531896d428049051a84d99f
Jeff Carley <jeff@thecarleys.net>
2018-06-01 00:07:40 -0600

    Add a component to show example work in a modal

---
+++ __tests__/test-example-work.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ExampleWork, { ExampleWorkBubble } from '../js/example-work';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const myWork = [
+  {
+    'title': "Work Example",
+    'image': {
+      'desc': "example screenshot of a project involving code",
+      'src': "images/example1.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Work Example",
+    'image': {
+      'desc': "example screenshot of a project involving chemistry",
+      'src': "images/example2.png",
+      'comment': `“Chemistry” by Surian Soosay is licensed under CC BY 2.0
+                   https://www.flickr.com/photos/ssoosay/4097410999`
+    }
+  }
+]
+describe("ExampleWork component", () => {
+  let component = shallow(<ExampleWork work={myWork}/>);
+
+  it("Should be a 'span' element", () => {
+    expect(component.type()).toEqual('span');
+  });
+
+  it("Should contain as many children as there are work examples", () => {
+    expect(component.find("ExampleWorkBubble").length).toEqual(myWork.length);
+  });
+
+  it("Should allow the modal to open and close", () => {
+    component.instance().openModal();
+    expect(component.instance().state.modalOpen).toBe(true);
+
+    component.instance().closeModal();
+    expect(component.instance().state.modalOpen).toBe(false);
+  });
+
+});
+
+describe("ExampleWorkBubble component", () => {
+  {/*
+  console.log(shallow(<ExampleWork work={myWork}/>).debug());
+  console.log(shallow(<ExampleWorkBubble example={myWork[1]}/>).debug());
+  */}
+  let mockOpenModalFn = jest.fn();
+
+  let component = shallow(<ExampleWorkBubble example={myWork[1]}
+    openModal={mockOpenModalFn}/>);
+
+  let images = component.find("img");
+
+  it("Should contain a single 'img' element", () => {
+    expect(images.length).toEqual(1);
+  });
+
+  it("Should call the openModal handler when clicked", () => {
+    component.find(".section__exampleWrapper").simulate('click');
+    expect(mockOpenModalFn).toHaveBeenCalled();
+  });
+
+});
--- webpack.config.js
+++ webpack.config.js
@@ -17,5 +17,8 @@ module.exports = {
         }
       }
     }]
-  }
+  },
+
+  mode: 'development'
+  
 }
